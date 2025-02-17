2d97cdee6c1f00a2eb5737bab9ab62268ae14208
RMartinOscar <40749467+RMartinOscar@users.noreply.github.com>
2025-02-17 17:27:10 +0000

    first commit

---
+++ src/Providers/WhmcsOauthProviderPluginProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace RMartinOscar\WhmcsOauthProvider\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class WhmcsOauthProviderPluginProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
